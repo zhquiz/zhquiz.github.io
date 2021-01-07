9cb6ae5cd64a756a0964e7b78dcde75a773228a3
Pacharapol Withayasakpunt <polv@polv.cc>
2021-01-07 21:50:35 +0700

    update

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const fs = require('fs')
 const yaml = require('js-yaml')
+const ghPage = require('gh-pages')
 
 fs.writeFileSync('www/libraryx.json', JSON.stringify(yaml.load(fs.readFileSync('../../assets/libraryx.yaml', 'utf8'))))
+
+ghPage.publish('www')
